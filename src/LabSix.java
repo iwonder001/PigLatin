623dedeeb5702769fb5db27dcdcc82ee48d76bbd
Kristen Carethers <kcarethers@gmail.com>
2016-09-14 11:39:39 -0400

    Initial commit

---
+++ src/LabSix.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class LabSix {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//download scanner class and set temp value to perm value
+		Scanner input = new Scanner(System.in);
+		
+		//do welcome
+		System.out.println("Welcome to Pig Latin Translator!");
+		//PUT THIS IN A LOOP!!!
+		//get user input of a line to be translated (put in loop)
+		System.out.println("Enter a line to be translated: ");
+		String translate = input.nextLine();
+		
+		//convert all text to lower case
+		//do if/else loop to check if begins with a vowel
+		//if begins with vowel add "way'' to end
+		// else then a constant
+		//nestle a for loop into constant to check for each vowel separately and if not a vowel then then add to end 
+		//of the word, and keep looping until hit a vowel then end the loop (break).
+		//ask if want to go again.
+		
+		
+	}//psvm
+
+}//class
